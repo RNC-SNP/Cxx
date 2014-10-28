58eec8ee9edffc51f27eadf0631ff6f20d723b59
RincLiu <i@RincLiu.com>
2014-10-27 21:14:55 -0700

    Vector.

---
+++ _base/vector.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+int main (int argc, char *argv[]) {
+	//init vector:
+	vector<char> v1 = {'1', '3', '5', '7', '9'};
+	vector<char> v2{'0', '2', '4', '6', '8'};
+	vector<char> v3(10);//init with count
+	vector<char> v4(10, 'x');//init with count and element
+	vector<char> v5(v1);//copy elements
+
+	//add elements:
+	vector<char> v6;
+	for (int i = 0; i < 26; i++) {
+		v6.push_back((char)('A' + i));
+	}
+
+	//visit elements:
+	for (auto &e : v6) {
+		e = (char)(e + 32);
+	}
+	for (auto e : v6) {
+		cout << e << endl;
+	}
+
+	//Vector:
+	for (auto it = v6.begin(); it != v6.end(); it++) {
+		cout << *it << endl;
+	}
+
+	return 0;
+}
