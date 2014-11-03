db7d6a819aba49221cfd593c15203d4be98079ff
RincLiu <i@RincLiu.com>
2014-11-02 20:17:20 -0800

    shared_ptr.

--- _base/shared_ptr.cxx
+++ _base/shared_ptr.cxx
@@ -7,14 +7,16 @@ using namespace std;
 int main (int argc, char **argv) {
 	//NOTE: shared_ptrs Automatically Destroy Their Objects
 
-	//shared_ptr<string> sps = make_shared<string>();
+	//shared_ptr<string> sp(new string());
+	//shared_ptr<string> sp = make_shared<string>();
 	auto sp = make_shared<string>();
 
 	if (sp && sp->empty()) {
 		*sp = "Rinc";
 	}
 
-	cout << *sp << endl;
+	string *sq = sp.get();//returns a built-in pointer to the object that the smart pointer is managing
+	cout << *sq << endl;
 
 	return 0;
 }
