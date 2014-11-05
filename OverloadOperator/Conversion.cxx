3e59b07670b5d8fb5c31a6c9c99d0860f7ebf1b6
RincLiu <i@RincLiu.com>
2014-11-04 18:57:19 -0800

    Conversion.

---
+++ OverloadOperator/Conversion.cxx
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+class Double {
+public:
+	Double(double d = 0):x(d){}
+	operator double () const {return x;} 
+private:
+	double x;
+};
+
+int main (int argc, char **argv) {
+	Double d(1.234);
+	d = 5.678;//implicitly converts 5.678 to Double
+	cout << (d + 0.0009) <<endl;//implicitly converts d to double
+	return 0;
+}
