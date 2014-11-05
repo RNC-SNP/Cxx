e36c54e21d2fbc0f071bcb13f0cd2828e8c2bdcf
RincLiu <i@RincLiu.com>
2014-11-04 18:58:48 -0800

    Conversion.

--- OverloadOperator/Conversion.cxx
+++ OverloadOperator/Conversion.cxx
@@ -13,6 +13,6 @@ private:
 int main (int argc, char **argv) {
 	Double d(1.234);
 	d = 5.678;//implicitly converts 5.678 to Double
-	cout << (d + 0.0009) <<endl;//implicitly converts d to double
+	cout << (d + 0.0009) << endl;//implicitly converts d to double
 	return 0;
 }
