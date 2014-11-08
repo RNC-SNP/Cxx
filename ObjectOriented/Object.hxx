e255d193c8d3cf3bf5525cd1ea8dbb11305f1563
Rinc <i@rincliu.com>
2014-11-08 15:04:08 +0800

    pure virtual function.

--- ObjectOriented/Object.hxx
+++ ObjectOriented/Object.hxx
@@ -2,11 +2,11 @@
 #define OBJECT_HXX
 
 class Object {
-	public:
-		Object() = default;
-		//declare a pure virtual function. So 'Object' is a abstract class.
-		virtual void print() const = 0;
-		virtual ~Object() = default;//Base class usually has a virtual destructor.
+public:
+	Object() = default;
+	//declare a pure virtual function. So 'Object' is a abstract class.
+	virtual void print() const = 0;
+	virtual ~Object() = default;//Base class usually has a virtual destructor.
 };
 
 #endif
