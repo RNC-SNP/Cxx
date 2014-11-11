4a587721b0df092fb00aec5535642553ff7e488c
RincLiu <i@RincLiu.com>
2014-11-10 23:52:14 -0800

    capture variables out of the lambda.

--- Functional/lambda.cxx
+++ Functional/lambda.cxx
@@ -2,8 +2,16 @@
 #include <functional>
 
 int main (int argc, char **argv) {
-	std::function<double(double, double)> multiply;//Use 'function' to declare a function pointer(C++11)
-	multiply = [](double x, double y){return x * y;};//Use lambda expression
+	//Use 'function' to declare a function pointer(C++11):
+	std::function<double(double, double)> multiply;
+	//Use lambda expression:
+	multiply = [](double x, double y){return x * y;};
 	std::cout << multiply(1.234, 5.6789) << std::endl;
+
+	double pi = 3.1415926535;
+	//Use '[&]' to capture a variable out of the lambda:
+	auto circleSpace = [&](double radius){return pi * radius * radius;};
+	std::cout << circleSpace(1.23456789) << std::endl;
+
 	return 0;
 }
