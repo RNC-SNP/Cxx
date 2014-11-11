e3d83dd4b2cea604b61d3cd1b9fe6440b0bffb7c
RincLiu <i@RincLiu.com>
2014-11-11 00:54:14 -0800

    Capture and assign variables outside of the lambda.

--- Functional/lambda.cxx
+++ Functional/lambda.cxx
@@ -4,15 +4,22 @@
 #include <algorithm>
 
 int main (int argc, char **argv) {
-	//Use 'function' to declare a function pointer(C++11):
+	//Use 'std::function' to declare a function pointer(C++11):
 	std::function<double(double, double)> multiply;
-	//Use lambda expression:
-	multiply = [] (double x, double y) -> double { return x * y; };
+	//Define lambda(C++11):
+	multiply = [] (double x, double y) -> double { return x * y; };//So, a lambda's type is 'std::function'
 	std::cout << multiply(1.234, 5.6789) << std::endl;
 
-	double pi = 3.1415926535;
-	//Use '[&]' to capture a variable outside of the lambda:
-	auto circleSpace = [&] (double radius) -> double { return pi * radius * radius; };
+	double pi = 3.14;
+	/*
+	Capture variables outside of the lambda:
+	'[]':			Capture nothing;
+	'[=]':			Capture any referenced variable by making a copy, the variable cannot be modified;
+	'[&]':			Capture any referenced variable by reference, the variable can be modified if not 'const';
+	'[=, &var]':	Capture any referenced variable by making a copy, but capture variable 'var' by reference;
+	'[this]':		Capture the 'this' pointer;
+	*/
+	auto circleSpace = [=, &pi] (double radius) -> double { pi = 3.1415926535; return pi * radius * radius; };
 	std::cout << circleSpace(1.23456789) << std::endl;
 
 	std::vector<char> v;
