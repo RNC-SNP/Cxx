942705c04be69edb4c3ed65a206f94409dca1cbe
RincLiu <i@RincLiu.com>
2014-11-12 19:29:34 -0800

    array

--- Container/array.cxx
+++ Container/array.cxx
@@ -3,12 +3,12 @@
 
 int main(int argc, char **argv) {
 	const int size = 26;
-    std::array<char, size> a;//declare a 'std::array'(C+=11)
-    for (int i = 0; i < size; i++) {
-    	a[i] = (char)('A' + i);//use 'std::array' as traditional array
-    }
-    for(char c : a) {
-    	std::cout << c << std::endl;
-    }
-    return 0;
+	std::array<char, size> a;//declare a 'std::array'(C+=11)
+	for (int i = 0; i < size; i++) {
+		a[i] = (char)('A' + i);//use 'std::array' as traditional array
+	}
+	for(char c : a) {
+		std::cout << c << std::endl;
+	}
+	return 0;
 }
