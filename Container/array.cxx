83e0c5671b1e358450a057ecc4d1c60d571e2b6f
RincLiu <i@RincLiu.com>
2014-11-12 19:05:36 -0800

    std::array

---
+++ Container/array.cxx
@@ -0,0 +1,14 @@
+#include <array>
+#include <iostream>
+
+int main(int argc, char **argv) {
+	const int size = 26;
+    std::array<char, size> a;//declare a 'std::array'
+    for (int i = 0; i < size; i++) {
+    	a[i] = (char)('A' + i);//use 'std::array' as traditional array
+    }
+    for(char c : a) {
+    	std::cout << c << std::endl;
+    }
+    return 0;
+}
