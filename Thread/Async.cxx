486db2192ebbbde19296a822bf562ec349b96ab7
Rinc Liu <i@Rinc.site>
2017-01-10 21:59:09 +0800

    constexpr

--- Thread/Async.cxx
+++ Thread/Async.cxx
@@ -4,13 +4,15 @@
 #include <vector>
 #include <iostream>
 
+float PI = atan(1.0f) * 4.0f;
+constexpr int count = 28;
+
 int main(int argc, char **argv) {
   std::vector<std::future<double>> results;
   std::function<double(double)> func = [](double radius) -> double {
-    float PI = atan(1.0f) * 4.0f;
 		return PI * radius * radius;
 	};
-  for (int i = 0; i < 28; i++) {
+  for (int i = 0; i < count; i++) {
     results.push_back(std::async(func, i));
   }
   for (auto &result : results) {
