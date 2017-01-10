9580fa033dcff1378911092fc0dbc3242bc32b86
Rinc Liu <i@Rinc.site>
2017-01-10 22:00:39 +0800

    format

--- Thread/Async.cxx
+++ Thread/Async.cxx
@@ -10,8 +10,8 @@ constexpr int count = 28;
 int main(int argc, char **argv) {
   std::vector<std::future<double>> results;
   std::function<double(double)> func = [](double radius) -> double {
-		return PI * radius * radius;
-	};
+    return PI * radius * radius;
+  };
   for (int i = 0; i < count; i++) {
     results.push_back(std::async(func, i));
   }
