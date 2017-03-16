f0fb4dc5aa2dbc3e82ae89b95855be6fac849b89
Rinc Liu <i@rinc.site>
2017-03-16 12:23:02 +0800

    Delete hello.cxx

--- hello.cxx
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-
-int main (int argc, char **argv) {
-	std::string firstName, familyName;
-	std::cin >> firstName >> familyName;
-	std::cout << "Hello, " << firstName << " " << familyName << "." << std::endl;
-	return 0;
-}
