6879d12075489e62fadc21ac211196bc70f9f9c1
Rinc Liu <rincliu@tencent.com>
2019-09-20 13:51:35 +0800

    Update SystemClock.cxx

--- DateTime/SystemClock.cxx
+++ DateTime/SystemClock.cxx
@@ -5,14 +5,14 @@
 #include <iomanip>
 
 std::string currentDatetimeStr(const char* format) {
-    auto now = std::chrono::system_clock::now();
-    auto dt = std::chrono::system_clock::to_time_t(now);
-    std::stringstream ss;
-    ss << std::put_time(std::localtime(&dt), format);
-    return ss.str();
+	auto now = std::chrono::system_clock::now();
+	auto dt = std::chrono::system_clock::to_time_t(now);
+	std::stringstream ss;
+	ss << std::put_time(std::localtime(&dt), format);
+	return ss.str();
 }
 
 int main(int argc, char **argv) {
-    std::cout << currentDatetimeStr("%Y-%m-%d %X") << std::endl;
+	std::cout << currentDatetimeStr("%Y-%m-%d %X") << std::endl;
 	return 0;
 }
